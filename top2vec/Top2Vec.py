b33388d2b95664724cfcb3892431ef30bad6373b
ncgovindans <86589392+ncgovindans@users.noreply.github.com>
2022-12-15 08:54:16 +1100

    Add embedding required

--- top2vec/Top2Vec.py
+++ top2vec/Top2Vec.py
@@ -64,6 +64,7 @@ use_model_urls = {
 
 sbert_models = ["distiluse-base-multilingual-cased",
                 "all-MiniLM-L6-v2",
+                "stsb-mpnet-base-v2",
                 "paraphrase-multilingual-MiniLM-L12-v2"]
 
 acceptable_embedding_models = use_models + sbert_models
